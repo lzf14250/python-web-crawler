a2975d44beea1d2a299c63ddb8e1bf05d3adbf6a
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-04-22 19:48:18 -0500

    submit updated file

--- practice/db1.py
+++ practice/db1.py
@@ -1,14 +1,34 @@
 import sqlite3
-import urllib.request
 import ssl
+import re
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+conn = sqlite3.connect('C:\\Users\\lzf95\\Desktop\\PythonPrac\\db1.sqlite')
+cur = conn.cursor()
+file_path = 'C:\\Users\\lzf95\\Desktop\\PythonPrac\\mbox.txt'
 
-cont = urllib.request.urlopen('https://www.py4e.com/code3/mbox.txt',context=ctx).decode()
-conn = sqlite3.connect('db1.db')
-ctr = conn.cursor()
+cur.execute('drop table if exists Counts')
 
-for i in cont:
-    print(i)
+cur.execute('create table Counts(org TEXT, count INTEGER)')
+
+with open(file_path,'r') as f:
+    for line in f:
+        catched = re.findall('^From .*?@(.*?) ',line)
+        if not catched: continue
+        domain = catched[0]
+        cur.execute('select * from Counts where org = ?',(domain,))
+        row = cur.fetchone()
+        if row is None:
+            cur.execute('insert into Counts (org,count) values (?,1)',(domain,))
+        else:
+            cur.execute('update Counts set count = count + 1 where org = ? ',(domain,))
+    conn.commit()
+    f.close()
+
+sqlstr = 'SELECT org,count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+    
+print('finished !')
+
+cur.close()
