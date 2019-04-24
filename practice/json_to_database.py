072cb589052281a385190b0fbbe6178ff9920db5
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-04-23 23:44:11 -0500

    first commit

---
+++ practice/json_to_database.py
@@ -0,0 +1,36 @@
+import json
+import sqlite3
+
+db_path = 'C:\\Users\\lzf95\\Desktop\\PythonPrac\\db2.sqlite'
+file_path = 'C:\\Users\\lzf95\\Desktop\\PythonPrac\\files\\roster_data.json'
+conn = sqlite3.connect(db_path)
+cur = conn.cursor()
+
+## create the tables
+cur.executescript('''
+DROP IF EXISTS User;
+DROP IF EXISTS Member;
+DROP IF EXISTS Course;
+
+CREATE TABLE User(
+    id INTEGER PRIMRARY KEY NOT NULL AUTOINCREMENT UNIQUE,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE Course(
+    id INTEGER PRIMRARY KEY NOT NULL AUTOINCREMENT UNIQUE,
+    title TEXT UNIQUE
+)
+
+CREATE TABLE Member(
+    user_id INTEGER, 
+    course_id INTEGER,
+    role INTEGER,
+    PRIMRARY KEY(user_id, course_id)
+)
+''')
+
+file_content = json.loads(open(file_path).read())
+
+for ncr in file_content:
+    
