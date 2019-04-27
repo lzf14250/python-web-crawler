03f1c34eb399361c889a0d38cc52099e4148a3c2
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-04-27 12:48:24 -0500

    editted

--- pagerank/rank.py
+++ pagerank/rank.py
@@ -6,3 +6,21 @@ apply the pagerank algorithm to the links.
 import pandas as pd
 import numpy as np
 import sqlite3
+
+conn = sqlite3.connect('pagerank.sqlite')
+cur = conn.cursor()
+
+from_list = []
+to_list = []
+## start retrieve the links from database
+cur.execute('SELECT from_id,to_id FROM Links')
+for (from_id,to_id) in cur:
+    from_list.append(from_id)
+    to_list.append(to_id)
+## remove the repeated links
+pairs = set(zip(from_list,to_list))
+from_to = list(zip(*pairs))
+
+from_list, to_list = from_to[0], from_to[1]
+print(from_list)
+print(to_list)
